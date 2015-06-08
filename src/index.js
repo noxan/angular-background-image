5025896f7622a31f778fd1c634ab1f44d5ab9fe1
Richard <richard@devisio.net>
2015-06-08 20:01:02 +0200

    implement background image directive
    based on http://stackoverflow.com/a/15537359

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 angular.module("backgroundImage", []);
+
+angular.module("backgroundImage").directive("backgroundImage", [function () {
+  return {
+    link: function (scope, element, attrs) {
+      attrs.$observe("backgroundImage", function (src) {
+        element.css({
+          "background-image": "url('" + src + "')"
+        });
+      });
+    }
+  };
+}]);
